363e11bd38c15a9b4672b6ebcfb34254e7d1c3b0
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2020-12-03 08:50:39 -0600

    portability schmortability

--- arraylist.h
+++ arraylist.h
@@ -15,13 +15,13 @@ void alist_init_typeless(alist *list, size_t width, size_t initcap);
 void alist_free(alist *list);
 
 void alist_set(alist *list, size_t pos, const void *data);
-#define alist_set_imm(type, list, pos, val) { type _data = (val); alist_set((list), (pos), &_data); }
+#define alist_set_imm(type, list, pos, val) do { type _data = (val); alist_set((list), (pos), &_data); } while (0)
 void *alist_get_typeless(const alist *list, size_t pos);
 #define alist_get(type, list, pos) (*(type *) alist_get_typeless((list), (pos)))
 
 void alist_resize(alist *list, size_t target);
 void alist_add(alist *list, const void *data);
-#define alist_add_imm(type, list, val) { type _data = (val); alist_add((list), &_data); }
+#define alist_add_imm(type, list, val) do { type _data = (val); alist_add((list), &_data); } while (0)
 void alist_remove(alist *list);
 
 #endif
--- arraylist_test.c
+++ arraylist_test.c
@@ -3,16 +3,17 @@
 
 int main() {
 	alist list;
+	int i = 41;
 	alist_init(int, &list);
 
 	alist_add_imm(int, &list, 10);
 	alist_add_imm(int, &list, 20);
 	alist_add_imm(int, &list, 30);
-	int i = 41;
 	alist_add(&list, &i);
 
 	printf("list[1] = %d\n", alist_get(int, &list, 1));
 	printf("list[3] = %d\n", alist_get(int, &list, 3));
 
 	alist_free(&list);
+	return 0;
 }
